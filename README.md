e9be1da2ed74805d847e83acadb7f40ad91ee747
awachtler <axel@uracoli.de>
2020-09-19 07:38:19 +0200

    initial add

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# devpi-json-info
+# devpi-json-info - JSON Info Plugin for devpi
+
+## Origin
+
+This plugin is based on a work @fschulze based on a question on
+stackoverflow: https://stackoverflow.com/questions/63711923/devpi-how-to-get-root-package-json-served?noredirect=1#comment113022032_63711923
+
+## Purpose
+
+The plugin serves the infos for a Python package in JSON format.
+This feature is e.g. used by micropython-upip.
+
+
---
+++ devpi_json_info.py
@@ -0,0 +1,39 @@
+from devpi_common.url import URL
+from devpi_server.readonly import get_mutable_deepcopy
+from pluggy import HookimplMarker
+from pyramid.view import view_config
+
+
+devpiserver_hookimpl = HookimplMarker("devpiserver")
+
+
+@devpiserver_hookimpl
+def devpiserver_pyramid_configure(config, pyramid_config):
+    # by using include, the package name doesn't need to be set explicitly
+    # for registrations of static views etc
+    pyramid_config.include('devpi_json_info')
+
+
+def includeme(config):
+    config.add_route(
+        "json_info",
+        "/{user}/{index}/{project}/json")
+    config.scan()
+
+
+@view_config(
+    route_name="json_info",
+    accept="application/json",
+    request_method="GET",
+    renderer='json')
+def json_info_view(context, request):
+    baseurl = URL(request.application_url).asdir()
+    version = context.stage.get_latest_version(context.project, stable=True)
+    info = get_mutable_deepcopy(
+        context.stage.get_versiondata(context.project, version))
+    info.pop('+elinks', None)
+    result = dict(info=info, releases={})
+    for release in context.stage.get_releaselinks(context.project):
+        result['releases'].setdefault(release.version, []).append(dict(
+            url=baseurl.joinpath(release.relpath).url))
+    return result
